34927ff9a1ea88a57f5f7d2605d0b1c12e1e03df
Haseeb-Khalil <haseeb3927@gmail.com>
2023-02-13 17:11:10 +0000

    Basic File Read and Transfer using Apache Camel

---
+++ src/main/java/com/camel/apache/apacheCamel/FileMoverRoute.java
@@ -0,0 +1,17 @@
+package com.camel.apache.apacheCamel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+// Commented this so we can make it work with XML Version
+
+//@Component
+//public class FileMoverRoute extends RouteBuilder {
+//    @Override
+//    public void configure() throws Exception {
+//        from("file:tmp/camel")
+//                .log("${body}")
+//                .to("file:tmp/camel/processed")
+//        ;
+//    }
+//}
