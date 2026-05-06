eb59ddb1249442429b5ab27be9dff4417effd32d
Haseeb Khalil <86537893+Haseeb-Khalil@users.noreply.github.com>
2026-05-06 10:16:33 +0000

    Enable Java DSL route under java-dsl profile so both DSLs can be exercised

--- src/main/java/com/camel/apache/apacheCamel/FileMoverRoute.java
+++ src/main/java/com/camel/apache/apacheCamel/FileMoverRoute.java
@@ -1,17 +1,26 @@
 package com.camel.apache.apacheCamel;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-// Commented this so we can make it work with XML Version
-
-//@Component
-//public class FileMoverRoute extends RouteBuilder {
-//    @Override
-//    public void configure() throws Exception {
-//        from("file:tmp/camel")
-//                .log("${body}")
-//                .to("file:tmp/camel/processed")
-//        ;
-//    }
-//}
+/**
+ * File-mover route in Java DSL.
+ *
+ * Active when the "java-dsl" Spring profile is enabled. By default the project
+ * runs with the equivalent XML DSL route in src/main/resources/camel-routes/routes.xml,
+ * so the two implementations can be compared side by side.
+ *
+ * Run with Java DSL:
+ *   ./mvnw spring-boot:run -Dspring-boot.run.profiles=java-dsl
+ */
+@Component
+@Profile("java-dsl")
+public class FileMoverRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("file:tmp/camel?noop=false")
+                .log("Java DSL picked up file: ${header.CamelFileName}, body: ${body}")
+                .to("file:tmp/camel/processed");
+    }
+}
